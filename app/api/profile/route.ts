8c1807bbb7ff0989aad03ed92cac3d98aa63d7a6
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-23 21:11:13 +0100

    🛠 fixing deployment issue

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,29 +1,25 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-	try {
-		const accessToken = request.headers.get("Authorization");
+	const accessToken = request.headers.get("Authorization");
 
-		if (!accessToken) {
-			return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-		}
-
-		const response = await fetch("https://api.spotify.com/v1/me", {
-			headers: {
-				Authorization: accessToken
-			}
-		});
-
-		console.log(accessToken);
-
-		const data = await response.json();
-
-		return NextResponse.json({
-			hello: "My request works",
-			accessToken: accessToken,
-			data
-		});
-	} catch (error) {
-		return NextResponse.error();
+	if (!accessToken) {
+		return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
 	}
+
+	const response = await fetch("https://api.spotify.com/v1/me", {
+		headers: {
+			Authorization: accessToken
+		}
+	});
+
+	console.log(accessToken);
+
+	const data = await response.json();
+
+	return NextResponse.json({
+		hello: "My request works",
+		accessToken: accessToken,
+		data
+	});
 }
