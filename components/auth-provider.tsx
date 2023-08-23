d7262b886aa67872badeb4e89b0241d547cfd940
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-23 09:15:41 +0100

    🛠 fixing redirect issue

--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -5,13 +5,16 @@ import React from "react";
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 	const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string;
 	const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string;
-	const appBaseUrl = process.env.NEXT_PUBLIC_APP_BASE_URL;
+	const redirectUri =
+		process.env.NODE_ENV === "development"
+			? (process.env.DEV_REDIRECT as string)
+			: (process.env.PROD_REDIRECT as string);
 
 	return (
 		<Auth0Provider
 			domain={domain}
 			clientId={clientId}
-			authorizationParams={{ redirect_uri: `${appBaseUrl}/home` }}
+			authorizationParams={{ redirect_uri: redirectUri }}
 		>
 			{children}
 		</Auth0Provider>
