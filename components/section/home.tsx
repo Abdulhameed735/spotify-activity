f526b2be2d6417445f4b8e091d04844469a8f003
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-26 00:21:05 +0100

    ✨ adding avatar - (prod test)

--- components/section/home.tsx
+++ components/section/home.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useSession, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const HomePage = () => {
 	const { data: session } = useSession();
@@ -14,6 +15,9 @@ const HomePage = () => {
 	return (
 		<div className="container">
 			<h1>Welcome, {user?.name}!</h1>
+			<Avatar>
+				<AvatarImage src={user?.image as string}></AvatarImage>
+			</Avatar>
 			<Button className="rounded-lg bg-[#1db954] hover:bg-[#1ed655be]" onClick={() => signOut()}>
 				Sign out
 			</Button>
