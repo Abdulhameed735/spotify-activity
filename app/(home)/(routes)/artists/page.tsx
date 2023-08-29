3ea0796190855b008a4a2b97853014a94dd4b4d0
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-29 21:33:00 +0100

    ✨ starting top artists page

--- app/(home)/(routes)/artists/page.tsx
+++ app/(home)/(routes)/artists/page.tsx
@@ -1,7 +1,22 @@
 const TopArtists = () => {
 	return (
-		<div>
-			<h1>Top Artists</h1>
+		<div className="flex h-full flex-col gap-y-16 p-3 lg:p-5">
+			<header className="block items-center  justify-stretch gap-y-4 lg:flex lg:flex-row  lg:justify-between">
+				<h2 className="text-center text-xl font-bold lg:text-2xl">Top Artists</h2>
+				<div className="flex justify-around gap-3">
+					<button className="bg-transparent p-2 font-semibold">
+						<span>All Time</span>
+					</button>
+
+					<button className="bg-transparent p-2 font-semibold">
+						<span>Last 6 months</span>
+					</button>
+
+					<button className="bg-transparent p-2 font-semibold">
+						<span>Last 4 weeks</span>
+					</button>
+				</div>
+			</header>
 		</div>
 	);
 };
