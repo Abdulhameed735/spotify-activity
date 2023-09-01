8c79f1533b30d6bee856353b0c3199a7a79dc804
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-09-01 17:05:36 +0100

    ✨

--- app/(home)/(routes)/tracks/[id]/page.tsx
+++ app/(home)/(routes)/tracks/[id]/page.tsx
@@ -32,42 +32,7 @@ const TracksPage = ({ params }: { params: { id: string } }) => {
 		fetchTracksData();
 	}, [status, session]);
 
-	return (
-		<div className="flex h-full flex-col gap-y-16 p-3 lg:p-5">
-			{tracksData?.tracksInfo && (
-				<div className="flex gap-x-4">
-					<Link href={tracksData.tracksInfo.external_urls.spotify}>
-						<picture>
-							<img
-								className="h-[150px] w-[150px] object-cover md:h-[170px] md:w-[170px] lg:h-[190px] lg:w-[190px]"
-								src={tracksData.tracksInfo.album.images[0].url}
-								alt={tracksData.tracksInfo.album.name}
-							/>
-						</picture>
-					</Link>
-					<div className="flex-1 flex-col gap-y-4 text-lg font-semibold">
-						<h2 className="text-3xl">{tracksData.tracksInfo.album.name}</h2>
-						<h2 className="text-xl">
-							{tracksData.tracksInfo.artists.map((artist) => (
-								<span key={artist.id}>{artist.name}</span>
-							))}
-						</h2>
-						<div className="gap-1 text-sm text-slate-300">
-							<span>{tracksData.tracksInfo.album.name}</span>·
-							<span className="hover:underline">
-								{new Date(tracksData.tracksInfo.album.release_date).getFullYear()}
-							</span>
-						</div>
-						<Link href={tracksData.tracksInfo.external_urls.spotify}>
-							<Button className="lg:w-[150px]rounded-full w-full bg-green-600 hover:bg-green-700">
-								Play
-							</Button>
-						</Link>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+	return <div className="flex h-full flex-col gap-y-16 p-3 lg:p-5"></div>;
 };
 
 export default TracksPage;
