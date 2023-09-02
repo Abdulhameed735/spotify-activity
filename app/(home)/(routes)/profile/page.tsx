a1611beaeab0bbf900b4651467759314dee50123
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-09-02 16:57:17 +0100

    🔃 add loading state to profile page (test)

--- app/(home)/(routes)/profile/page.tsx
+++ app/(home)/(routes)/profile/page.tsx
@@ -13,6 +13,7 @@ const ProfilePage = () => {
 	const { data, status } = useSession();
 	const session = data as Session & { accessToken: string | null };
 	const [userProfile, setUserProfile] = useState<Profile | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		async function ProfileData() {
@@ -32,65 +33,67 @@ const ProfilePage = () => {
 		ProfileData();
 	}, [status, session]);
 
-	if (status === "loading") {
-		return <p>Loading...</p>;
-	}
-
 	return (
 		<div className="flex h-full flex-col gap-y-16 p-3 lg:p-5">
-			<header className="flex flex-col items-center gap-4">
-				{userProfile && userProfile.profile.images && (
-					<div>
-						<picture>
-							<img
-								src={userProfile.profile.images[1].url}
-								alt="User Profile Image"
-								className="h-[130px] w-[130px] rounded-full object-cover md:h-[170px] md:w-[170px] lg:h-[190px] lg:w-[190px]"
-							/>
-						</picture>
-					</div>
-				)}
-				{userProfile && (
-					<div className="flex flex-col gap-5">
-						<a
-							className="text-center"
-							target="_blank"
-							href={userProfile.profile.external_urls.spotify}
-						>
-							<h2 className="text-center text-4xl font-bold hover:text-[#1ed655be] lg:text-5xl">
-								{userProfile.profile.display_name}
-							</h2>
-						</a>
-
-						<div className="flex items-center gap-4">
-							<div className="flex flex-col items-center space-y-2">
-								<div className="text-xl font-bold">{userProfile.profile.followers.total}</div>
-								<p className="text-base uppercase leading-relaxed text-[#1db954]">Followers</p>
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				<>
+					<header className="flex flex-col items-center gap-4">
+						{userProfile && userProfile.profile.images && (
+							<div>
+								<picture>
+									<img
+										src={userProfile.profile.images[1].url}
+										alt="User Profile Image"
+										className="h-[130px] w-[130px] rounded-full object-cover md:h-[170px] md:w-[170px] lg:h-[190px] lg:w-[190px]"
+									/>
+								</picture>
 							</div>
-							<div className="flex flex-col items-center space-y-2">
-								<div className="text-xl font-bold">{userProfile.profile.followers.total}</div>
-								<p className="text-base uppercase leading-relaxed text-[#1db954]">Following</p>
+						)}
+						{userProfile && (
+							<div className="flex flex-col gap-5">
+								<a
+									className="text-center"
+									target="_blank"
+									href={userProfile.profile.external_urls.spotify}
+								>
+									<h2 className="text-center text-4xl font-bold hover:text-[#1ed655be] lg:text-5xl">
+										{userProfile.profile.display_name}
+									</h2>
+								</a>
+
+								<div className="flex items-center gap-4">
+									<div className="flex flex-col items-center space-y-2">
+										<div className="text-xl font-bold">{userProfile.profile.followers.total}</div>
+										<p className="text-base uppercase leading-relaxed text-[#1db954]">Followers</p>
+									</div>
+									<div className="flex flex-col items-center space-y-2">
+										<div className="text-xl font-bold">{userProfile.profile.followers.total}</div>
+										<p className="text-base uppercase leading-relaxed text-[#1db954]">Following</p>
+									</div>
+									<Link href="/playlist" className="flex flex-col items-center space-y-2">
+										<div className="text-xl font-bold">{userProfile.playlist.total}</div>
+										<p className="text-base uppercase leading-relaxed text-[#1db954]">Playlists</p>
+									</Link>
+								</div>
+
+								<Button
+									className="rounded-full border bg-transparent hover:border-[#1ed655be] hover:bg-[#1ed655be]"
+									onClick={() => signOut()}
+								>
+									Signout
+								</Button>
 							</div>
-							<Link href="/playlist" className="flex flex-col items-center space-y-2">
-								<div className="text-xl font-bold">{userProfile.playlist.total}</div>
-								<p className="text-base uppercase leading-relaxed text-[#1db954]">Playlists</p>
-							</Link>
-						</div>
+						)}
+					</header>
 
-						<Button
-							className="rounded-full border bg-transparent hover:border-[#1ed655be] hover:bg-[#1ed655be]"
-							onClick={() => signOut()}
-						>
-							Signout
-						</Button>
-					</div>
-				)}
-			</header>
-
-			<section className="grid w-full grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-10">
-				<TopArtists />
-				<TopTracks />
-			</section>
+					<section className="grid w-full grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-10">
+						<TopArtists />
+						<TopTracks />
+					</section>
+				</>
+			)}
 		</div>
 	);
 };
